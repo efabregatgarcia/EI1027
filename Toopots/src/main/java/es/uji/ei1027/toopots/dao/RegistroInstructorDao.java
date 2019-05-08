95cd45be928d12cd5090cafef471ee42a18ae7b7
Rosu <Rosu@DESKTOP-6T0AJOP>
2019-05-08 19:54:22 +0200

    RegistroInstructor realizado sin comprobacion

---
+++ Toopots/src/main/java/es/uji/ei1027/toopots/dao/RegistroInstructorDao.java
@@ -0,0 +1,65 @@
+package es.uji.ei1027.toopots.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import es.uji.ei1027.toopots.model.RegistroInstructor;
+
+@Repository
+public class RegistroInstructorDao {
+	
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	/* Añade el registro de instructor a la base de datos */
+	public void addRegistroInstructor(RegistroInstructor registroInstructor) {
+		jdbcTemplate.update("INSERT INTO registroInstructor VALUES(?, ?, ?, ?, ?, ?, ?)",
+				registroInstructor.getIdRegistro(), registroInstructor.getIdInstructor(), registroInstructor.getNombre(),
+				registroInstructor.getDireccion(),registroInstructor.getTelefono(),registroInstructor.getCurriculum(),registroInstructor.getAptitudes());
+	}
+
+	
+
+	public void deleteRegistroInstructor(String registroInstructor) {
+		jdbcTemplate.update("DELETE from registroInstructor where idRegistro=?", registroInstructor);
+	}
+
+	/*
+	 * Actualiza los atributos del registro instructor (menos la idRegistro
+	 * que es clave primarias e idInstructor que es clave ajena)
+	 */
+	public void updateRegistroInstructor(RegistroInstructor registroInstructor) {
+		jdbcTemplate.update("UPDATE registroInstructor SET nombre=?, direccion=?, telefono=?, curriculum=?, aptitudes=? WHERE idRegistro=?",
+				registroInstructor.getNombre(), registroInstructor.getDireccion(), registroInstructor.getTelefono(), registroInstructor.getCurriculum(),
+				registroInstructor.getAptitudes());
+	}
+
+	/* Obtiene el registro instructor a partir de su idRegistro. Devuelve nulo si no existe. */
+	public RegistroInstructor getRegistroInstructor(String idRegistro) {
+		try {
+			return jdbcTemplate.queryForObject("SELECT * from registroInstructor WHERE idRegistro=?", new RegistroInstructorRowMapper(), idRegistro);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+
+	/* Obtiene todos los registros de intructores. Devuelve una lista vacia si no existe. */
+	public List<RegistroInstructor> getRegistroInstructores() {
+		try {
+			return jdbcTemplate.query("SELECT * from registroInstructor", new RegistroInstructorRowMapper());
+		} catch (EmptyResultDataAccessException e) {
+			return new ArrayList<RegistroInstructor>();
+		}
+	}
+}
---
+++ Toopots/src/main/java/es/uji/ei1027/toopots/model/RegistroInstructor.java
@@ -0,0 +1,63 @@
+package es.uji.ei1027.toopots.model;
+
+public class RegistroInstructor {
+	
+	private String idRegistro;
+	private String idInstructor;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+	private String curriculum;
+	private String aptitudes;
+	
+
+	public String getIdRegistro() {
+		return idRegistro;
+	}
+	public void setIdRegistro(String idRegistro) {
+		this.idRegistro = idRegistro;
+	}
+	public String getIdInstructor() {
+		return idInstructor;
+	}
+	public void setIdInstructor(String idInstructor) {
+		this.idInstructor = idInstructor;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCurriculum() {
+		return curriculum;
+	}
+	public void setCurriculum(String curriculum) {
+		this.curriculum = curriculum;
+	}
+	public String getAptitudes() {
+		return aptitudes;
+	}
+	public void setAptitudes(String aptitudes) {
+		this.aptitudes = aptitudes;
+	}
+	
+	@Override
+	public String toString() {
+		return "RegistroInstructor [idRegistro=" + idRegistro + ", idInstructor=" + idInstructor + ", nombre=" + nombre
+				+ ", direccion=" + direccion + ", telefono=" + telefono + ", curriculum=" + curriculum + ", aptitudes="
+				+ aptitudes + "]";
+	}
+}
