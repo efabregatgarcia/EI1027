21e086fb074e1df4fddc91b3cb2ffc9d8684bf48
Rosu <Rosu@DESKTOP-6T0AJOP>
2019-05-09 19:23:10 +0200

    Login a medias

---
+++ Toopots/src/main/java/es/uji/ei1027/toopots/model/Usuario.java
@@ -0,0 +1,50 @@
+package es.uji.ei1027.toopots.model;
+
+import es.uji.ei1027.toopots.enumeracion.EnumTipoUsuario;
+
+public class Usuario {
+	String idUsuario;
+	String password; 
+	EnumTipoUusario enumTipo;
+	
+	public String getIdUsuario() {
+		return idUsuario; 
+	}
+
+	public void setIdUsuario(String idUsuario) {
+	    this.idUsuario = idUsuario; 
+	}
+
+	public String getPassword() {
+	   return password; 
+	}
+
+	public void setPassword(String password) {
+	   this.password = password;
+	}
+	public EnumTipoUsuario getEnumTipoUsuario() {
+		return this.enumTipo;
+		
+	}
+	public boolean isCliente() {
+		if (enumTipo == EnumTipoUsuario.CLIENTE) {
+			return true;
+		}
+		return false;
+	}
+	public boolean isInstructor() {
+		
+		if (enumTipo == EnumTipoUsuario.INSTRUCTOR) {
+			return true;
+		}
+		return false;
+	}
+	public boolean isAdministrador() {
+		
+		if (enumTipo == EnumTipoUsuario.Administrador) {
+			return true;
+		}
+		return false;
+	}	
+
+}
