5dcc8b04c5064514b31f07a8b2b2687bb3cfff0c
Rosu <Rosu@DESKTOP-6T0AJOP>
2019-05-16 15:32:07 +0200

    indexController

---
+++ Toopots/src/main/java/es/uji/ei1027/toopots/controller/IndexController.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.toopots.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class IndexController {
+	@RequestMapping("/")
+	public String index() {	
+		return "index";
+	}
+}
